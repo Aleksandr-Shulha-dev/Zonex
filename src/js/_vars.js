9f5c0026865a1d1b4095965a4c953298d89ca794
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2021-09-10 23:36:13 +0300

    Created header and hero block

---
+++ src/js/_vars.js
@@ -0,0 +1,4 @@
+export default {
+    $bannerSlider: document.querySelector('.banner-slider'),
+
+}
---
+++ src/js/components/main-slider.js
@@ -0,0 +1,13 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+import vars from '../_vars';
+
+const bannerSlider = new Swiper(vars.$bannerSlider, {
+    modules: [ Navigation, Pagination ],
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        bulletClass: 'banner-pag__bullet',
+    },
+});
